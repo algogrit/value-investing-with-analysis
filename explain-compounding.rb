3996a06369559416ca764af474f7b33a9d49d978
Gaurav Agarwal <algogrit@gmail.com>
2019-10-12 13:52:48 +0530

    Explaining compounding with a ruby script.

--- explain-compounding.rb
+++ explain-compounding.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/env ruby
 
-initial_amount = 100000
-rate = 7
+require 'humanize'
+
+initial_amount = 1_26_000
+rate = 9.0
 years = 10
 
 puts "Use defaults - Amount: #{initial_amount}, Rate: #{rate}, Years: #{years}?"
@@ -12,7 +14,7 @@ if default.include? "n"
   initial_amount = gets.to_i
 
   puts "Enter rate of interest: "
-  rate = gets.to_i
+  rate = gets.to_f
 
   puts "Enter total years: "
   years = gets.to_i
@@ -20,10 +22,11 @@ end
 
 amount = initial_amount
 
-puts "You invested #{amount}"
+puts "You invested \"#{amount.humanize}\" for #{years} years at #{rate}% compounded"
+
 years.times do |i|
-  amount = amount * ( 1 + (rate / 100.0) )
-  puts "At end of #{i+1} year: #{amount}"
+  amount = (amount * ( 1 + (rate / 100.0) )).round(2)
+  puts "At end of #{i+1} year: #{amount} or \"#{amount.humanize}\""
 end
 
 puts "If you invest it for another #{years} years: #{amount * (( 1 + (rate / 100.0) ) ** years)}"
